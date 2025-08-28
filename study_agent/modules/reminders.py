9c0d272258d8aec4303f85b60660d94a22de9e42
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2025-08-28 20:34:57 -0300

    Delete study_agent directory

--- study_agent/modules/reminders.py
+++
@@ -1,4 +0,0 @@
-def send_daily_reminder(schedule):
-    print("\nLembrete diario de estudo:")
-    for item in schedule:
-        print(f"- {item['daily_hours']} -> {item['subject']}: {item['topic']}")
